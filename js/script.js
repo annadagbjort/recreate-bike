c053d25c2aa6426d0065ac50537a22bd8958cd6d
Anna Dagbjort <annadagbjort@Annas-MacBook-Pro.local>
2020-04-03 14:16:04 +0000

    fetches and shows data

--- js/script.js
+++ js/script.js
@@ -4,4 +4,25 @@ fetch("http://annadagbjort.dk/cms-theme/bikes/wp-json/wp/v2/bike?_embed")
 
 function handleData(posts) {
     console.log(posts);
+    posts.forEach(showBikes)
+}
+
+function showBikes(bikes) {
+    console.log(bikes)
+    const template = document.querySelector("template").content;
+
+    const copy = template.cloneNode(true);
+
+    console.log(bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
+
+    copy.querySelector(".brand").textContent = bikes.brand;
+
+    copy.querySelector(".model").textContent = bikes.model;
+    copy.querySelector(".price").textContent = bikes.price;
+    copy.querySelector(".colour").textContent = bikes.colours;
+    copy.querySelector(".inStock").textContent = bikes.in_stock;
+    copy.querySelector(".img-bike").src = bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
+
+
+    document.querySelector("main").appendChild(copy);
 }
