22bd825cc00dfa2d7b6eda837b03be61e64074a9
Anna Dagbjort <annadagbjort@Annas-MacBook-Pro.local>
2020-04-05 18:52:34 +0000

    root colors, header, colours work!!!!

--- js/script.js
+++ js/script.js
@@ -20,13 +20,41 @@ function showBikes(bikes) {
     copy.querySelector(".model").textContent = bikes.model;
     copy.querySelector(".price").textContent = bikes.price;
     copy.querySelector(".toPrice").textContent = bikes.price_to;
-    copy.querySelector(".colour").textContent = bikes.colours;
+
+//    copy.querySelector(".colour").textContent = bikes.colours;
+
     copy.querySelector(".inStock").textContent = bikes.in_stock;
 
 
     copy.querySelector(".img-bike").src = bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
     copy.querySelector(".img-bike").alt = bikes.brand;
 
+    if (bikes.price_to == false) {
+        copy.querySelector(".twoPrices").classList.add("hide");
+    }
+
+    const colorArray = bikes.colours.split(",");
+
+    colorArray.forEach(color=>{
+        const col = document.createElement("div")
+        col.classList.add("colourDiv");
+        col.style.background=color;
+        copy.querySelector(".colour").appendChild(col)
+    })
+
+     if (bikes.colours == false) {
+        copy.querySelector(".colour").textContent = ("N/A");
+    }
+
 
     document.querySelector("main").appendChild(copy);
 }
+
+
+
+
+
+
+
+
+
